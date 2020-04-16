a83bc42e67cd5ef91b6663437bebb9333d4df13b
jirkafm <jiri.kaplan@email.cz>
2020-04-16 16:15:00 +0200

    Path for rsync fixed.

--- scripts/env.sh
+++ scripts/env.sh
@@ -2,13 +2,14 @@
 OVERWRITE=N
 CONFS_GIT_URL=https://github.com/jirkafm
 CONFS_GIT_REPO=my-env
+CONFS_GIT_ENV_DIR=$HOME/.jirkafm/$CONFS_GIT_REPO
 
-git clone $CONFS_GIT_URL/$CONFS_GIT_REPO $HOME/.jirkafm/$CONFS_GIT_REPO
+git clone $CONFS_GIT_URL/$CONFS_GIT_REPO $CONFS_GIT_ENV_DIR 
 
 # This can be very dangerous as it may overwrite your environment
 # settings if you are sure set OVERWRITE=Y to get dot files from
 # git repo to your $HOME
 if [[ ! -z $OVERWRITE && $OVERWRITE == 'Y' ]]; then
-  rsync --exclude '.git'  -avI $HOME/$CONFS_GIT_REPO/.[^.]* ~/
+  rsync --exclude '.git'  -avI $CONFS_GIT_ENV_DIR/.[^.]* ~/
 	chmod 600 /home/$USER/.ssh/config
 fi
