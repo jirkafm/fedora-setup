5cfe0dbc185b2255db290a45334c1128c92f9725
jirkafm <jiri.kaplan@email.cz>
2020-04-16 16:03:42 +0200

    If checks for zshell because of docker automation.

--- scripts/zshell.sh
+++ scripts/zshell.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-sudo $INSTALL_CMD zsh zsh-lovers
-sudo chsh -s /bin/zsh $USER 
+if [ ! -z $INSTALL_CMD ]; then
+	sudo $INSTALL_CMD zsh zsh-lovers
+fi
+
+if [ ! -z $USER ]; then
+	sudo chsh -s /bin/zsh $USER 
+fi
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
