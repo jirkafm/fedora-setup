ceef036512a22fb0272438090e0a72a1fa31ee3a
jirkafm <jiri.kaplan@email.cz>
2020-04-16 16:11:35 +0200

    my-env should be in .jirkafm

--- scripts/env.sh
+++ scripts/env.sh
@@ -3,7 +3,7 @@ OVERWRITE=N
 CONFS_GIT_URL=https://github.com/jirkafm
 CONFS_GIT_REPO=my-env
 
-git clone $CONFS_GIT_URL/$CONFS_GIT_REPO $HOME/$CONFS_GIT_REPO
+git clone $CONFS_GIT_URL/$CONFS_GIT_REPO $HOME/.jirkafm/$CONFS_GIT_REPO
 
 # This can be very dangerous as it may overwrite your environment
 # settings if you are sure set OVERWRITE=Y to get dot files from
