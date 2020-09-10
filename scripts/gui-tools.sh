907b328c43df9e25e790108bb73789eaa940d613
jirkafm <jiri.kaplan@email.cz>
2020-09-10 22:25:36 +0200

    Roboto fonts rpm

--- scripts/gui-tools.sh
+++ scripts/gui-tools.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo $INSTALL_CMD chromium qutebrowser redshift-gtk keepass pyxtrlock krusader kdiff3 krename shutter vlc xfreerdp tigervnc calibre filezilla vlc tilix
+sudo $INSTALL_CMD chromium qutebrowser redshift-gtk keepass pyxtrlock krusader kdiff3 krename shutter vlc xfreerdp tigervnc calibre filezilla vlc tilix google-roboto-fonts
